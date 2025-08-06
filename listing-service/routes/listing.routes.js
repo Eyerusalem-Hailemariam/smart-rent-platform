c70e24adb13832fcb476552858652acee495ad5d
Eyerusalem-Hailemariam <eyerusalem008@gmail.com>
2025-08-07 00:42:14 +0300

    configuring cloudinary.js

--- listing-service/routes/listing.routes.js
+++ listing-service/routes/listing.routes.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const {createListing, getListings, updateListing, deleteListing} = require('../controllers/listing.controller.js');
-
+const {storage} = require('../utils/cloudinary.js');
+const multer = require('multer');
 const router = express.Router();
+const upload = multer({ storage: storage });
 
 router.post('/', createListing);
 router.get('/', getListings);
 router.put('/:id', updateListing);
 router.delete('/:id', deleteListing);
+router.post('/upload', upload.single('image'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    res.status(200).send({
+        message: 'File uploaded successfully.',
+        image: req.file.path,
+        publicId : req.file.filename,
+    });
+});
 
 module.exports = router;
--- listing-service/utils/cloudinary.js
+++ listing-service/utils/cloudinary.js
@@ -1,9 +1,19 @@
 const {v2: cloudinary} = require('cloudinary');
+const {CloudinaryStorage} = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key : process.env,CLOUDINARY_API_KEY,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
     api_secret : process.env.CLOUDINARY_API_SECRET
 });
 
-module.exports = cloudinary;
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'smart-rent-platform',
+        allowed_formats: ['jpg', 'png', 'jpeg']
+    }
+});
+
+module.exports = { cloudinary, storage };
