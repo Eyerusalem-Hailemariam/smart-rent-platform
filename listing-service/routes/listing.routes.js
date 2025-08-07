181b863254a8f42d4a98660eede64898c197c561
Eyerusalem-Hailemariam <eyerusalem008@gmail.com>
2025-08-08 00:20:54 +0300

    correcting image upload

--- listing-service/routes/listing.routes.js
+++ listing-service/routes/listing.routes.js
@@ -1,23 +1,34 @@
 const express = require('express');
-const {create, getListings, updateListing, deleteListing} = require('../controllers/listing.controller.js');
-const {storage} = require('../utils/cloudinary.js');
 const multer = require('multer');
+const { create, getListings, updateListing, deleteListing } = require('../controllers/listing.controller');
+const { storage } = require('../utils/cloudinary');
+
 const router = express.Router();
-const upload = multer({ storage: storage });
 
 router.post('/', create);
 router.get('/', getListings);
 router.put('/:id', updateListing);
 router.delete('/:id', deleteListing);
+
+const upload = multer({ storage });
+
 router.post('/upload', upload.single('image'), (req, res) => {
-    if (!req.file) {
+
+    try {
+         if (!req.file) {
         return res.status(400).send('No file uploaded.');
     }
-    res.status(200).send({
+
+    res.status(200).json({
         message: 'File uploaded successfully.',
         image: req.file.path,
-        publicId : req.file.filename,
+        publicId: req.file.filename
     });
+    }catch(error){
+        console.error('Error uploading file:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+
 });
 
 module.exports = router;
--- listing-service/utils/cloudinary.js
+++ listing-service/utils/cloudinary.js
@@ -2,10 +2,11 @@ const {v2: cloudinary} = require('cloudinary');
 const {CloudinaryStorage} = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret : process.env.CLOUDINARY_API_SECRET
+  cloud_name: 'dgr325jcg',
+  api_key: '463334626256935',
+  api_secret: 'Yl9xliClUpZuo38J4pIya9EiGnk'
 });
+
 
 
 const storage = new CloudinaryStorage({
